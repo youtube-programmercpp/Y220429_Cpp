fb901ccd620ec752e027a9e18dc82ec05db4ad57
youtube-programmercpp <sbl908k@yahoo.co.jp>
2022-04-30 22:05:47 +0900

    かなり難しいレベルの内容を追加

--- ConsoleApplication1/Test05.cpp
+++ ConsoleApplication1/Test05.cpp
@@ -2,63 +2,107 @@
 #include <sstream>
 #include <map>
 #include <algorithm>
+#include <limits.h>
 
 struct host_address {
-	unsigned char a;
-	unsigned char b;
-	unsigned char c;
-	unsigned char d;
+	int address;
+	static constexpr unsigned short ipv4size = 4;
+	static_assert(sizeof address >= ipv4size, "Must be able to store IPv4 address");
 	bool operator==(const host_address& r) const noexcept
 	{
-		return a == r.a
-		&&     b == r.b
-		&&     c == r.c
-		&&     d == r.d
-		;
+		return address == r.address;
 	}
 };
 std::ostream& operator<<(std::ostream& ostm, const host_address& r)
 {
-	return ostm
-		<< static_cast<short>(r.a) << '.'
-		<< static_cast<short>(r.b) << '.'
-		<< static_cast<short>(r.c) << '.'
-		<< static_cast<short>(r.d);
+#define	first reinterpret_cast<const unsigned char*>(&r.address)
+	auto p = first;
+	ostm << static_cast<short>(*p);
+	while (++p < &first[host_address::ipv4size])
+		ostm << '.' << static_cast<short>(*p);
+#undef	first
+	return ostm;
 }
-void mode_0(const std::map<std::string, host_address>& host_ip)
+std::istream& operator>>(std::istream& istm, host_address& r)
 {
-	std::string hostmei;
-	std::cout << "ホスト名は？";
-	std::cin >> hostmei;
-
-	const auto it = host_ip.find(hostmei);
-	if (it == host_ip.end())
-		std::cout << hostmei << "は該当無し" << "\n";
-	else
-		std::cout << hostmei << "は" << it->second << "\n";
+	const auto read = [](std::istream& istm, unsigned char* p)->std::istream&
+	{
+		short value;
+		if (istm >> value) {
+			if (0 <= value && value <= UCHAR_MAX)
+				*p = static_cast<unsigned char>(value);
+			else
+				istm.setstate(std::ios_base::failbit);
+		}
+		return istm;
+	};
+#define	first reinterpret_cast<unsigned char*>(&r.address)
+	auto p = first;
+	read(istm, p);
+	while (++p < &first[host_address::ipv4size]) {
+		char ch;
+		if (istm >> ch) {
+			if (ch == '.')
+				read(istm, p);
+			else
+				istm.setstate(std::ios_base::failbit);
+		}
+		else
+			break;
+	}
+#undef	first
+	return istm;
 }
-void mode_1(const std::map<std::string, host_address>& host_ip)
-{
-	std::string ipaddr;
-	std::cout << "IPアドレスは？";
-	std::cin >> ipaddr;
+template<int x>struct mode;
 
-	const auto it = std::find_if(host_ip.begin(), host_ip.end(), [&ipaddr](const auto& r)
-		{
-			return (std::ostringstream() << r.second).str() == ipaddr;
-		});
-	if (it == host_ip.end())
-		std::cout << ipaddr << "は該当無し\n";
+template<>struct mode<0> {
+	using key_type = std::string;
+	static const char prompt[];
+
+	static const host_address* find(const std::map<std::string, host_address>& host_ip, const key_type& key_data)
+	{
+		const auto it = host_ip.find(key_data);
+		if (it == host_ip.end())
+			return nullptr;
+		else
+			return &it->second;
+	}
+};
+template<>struct mode<1> {
+	using key_type = host_address;
+	static const char prompt[];
+	static const std::string* find(const std::map<std::string, host_address>& host_ip, const key_type& key_data)
+	{
+		const auto it = std::find_if(host_ip.begin(), host_ip.end(), [&key_data](const auto& r)
+			{
+				return r.second == key_data;
+			});
+		if (it == host_ip.end())
+			return nullptr;
+		else
+			return &it->first;
+	}
+};
+const char mode<0>::prompt[] = "ホスト名は？";
+const char mode<1>::prompt[] = "IPアドレスは？";
+
+template<int x>void mode_x(const std::map<std::string, host_address>& host_ip)
+{
+	typename mode<x>::key_type key_data;
+	std::cout << mode<x>::prompt;
+	std::cin >> key_data;
+
+	if (const auto p = mode<x>::find(host_ip, key_data))
+		std::cout << key_data << "は" << *p << "\n";
 	else
-		std::cout << ipaddr << "は" << it->first << "\n";
+		std::cout << key_data << "は該当無し" << "\n";
 }
 int main(void)
 {
 	std::map<std::string, host_address> host_ip;
-
-	host_ip["host1"] = { 101, 102, 103, 104 };
-	host_ip["host2"] = { 105, 106, 107, 108 };
-	host_ip["host3"] = { 109, 110, 111, 112 };
+	std::istringstream("101.102.103.104") >> host_ip["host1"];
+	std::istringstream("105.106.107.108") >> host_ip["host2"];
+	std::istringstream("109.110.111.112") >> host_ip["host3"];
 
 	for (const auto& r : host_ip)
 		std::cout << r.first << ":" << r.second << std::endl;
@@ -70,8 +114,8 @@ int main(void)
 
 		static void (* const a[])(const std::map<std::string, host_address>&host_ip)
 			= 
-		{ mode_0
-		, mode_1
+		{ mode_x<0>
+		, mode_x<1>
 		}
 		;
 		if (0 <= x && x <= 1)
